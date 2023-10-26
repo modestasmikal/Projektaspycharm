85ead0fb5470d2ce6f2f158c05b2a030d11ca6d4
Modestas Mikalauskas <modestasmikal@gmail.com>
2023-10-26 09:53:35 +0300

    kalkuliatorius, prideta sudetis

--- kalkuliatorius.py
+++ kalkuliatorius.py
@@ -16,3 +16,13 @@ while True:
     if meniu_pasirinkimas == "q":
         print("BYE BYE")
         break
+
+    if meniu_pasirinkimas not in ("1", "2", "3", "4", "5"):
+        print("Tokio pasirinkimo nėra")
+        continue
+        # naudojam float - skaičius su kableliu
+    sk1 = float(input("Įveskite pirmąjį skaičių "))
+    sk2 = float(input("Įveskite antrąjį skaičių "))
+
+    if meniu_pasirinkimas == "1":
+        res = f"{sk1} + {sk2} = {sk1 + sk2}"
--- pirmas.py
+++ pirmas.py
@@ -1,2 +1,3 @@
 print("Hello new project")
 print("Prideta po pirmojo commit")
+print("Prideta po antrojo commit")
