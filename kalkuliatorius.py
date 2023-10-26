44cfbb354f3633defdf489bd62ab208df28b5cd2
Modestas Mikalauskas <modestasmikal@gmail.com>
2023-10-26 09:36:14 +0300

    kalkuliatorius, meniu

---
+++ kalkuliatorius.py
@@ -0,0 +1,18 @@
+while True:
+
+    print(
+        "------------ ""\n"
+        "1. Sudetis" "\n"
+        "2. Atimtis" "\n"
+        "3. Daugyba" "\n"
+        "4. Dalyba" "\n"
+        "5. Seka"   "\n"
+        "q - Iseiti""\n"
+
+        "---------------",
+    )
+    meniu_pasirinkimas = input(f"Pasirinkite jums norima funkcija \n >>>>")
+
+    if meniu_pasirinkimas == "q":
+        print("BYE BYE")
+        break
--- pirmas.py
+++ pirmas.py
@@ -1,1 +1,2 @@
 print("Hello new project")
+print("Prideta po pirmojo commit")
